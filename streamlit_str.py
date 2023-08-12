78db22015e00c10675ff36c68ee3be1a2465ccbb
rupesh <rupeshmahanty1@gmail.com>
2023-08-12 15:40:09 +0530

    first commit

---
+++ streamlit_str.py
@@ -0,0 +1,27 @@
+import streamlit as st 
+import pandas as pd
+import numpy as np 
+import streamlit as st 
+import pandas as pd 
+import pickle 
+print("success")
+
+df = pickle.load(open("movies_dict.pkl",'rb')) 
+similarity = pickle.load(open("similarity.pkl",'rb'))
+movies = pd.DataFrame(df) 
+
+
+# now lets create webpage using the streamlit
+st.title("Movie Recommender System") 
+
+selected_title = st.selectbox(
+    "Please , Choose a movie",
+    movies['title'].values
+)
+
+movie_index = movies[movies['title']==selected_title].index[0]
+similar_movies = sorted(list(enumerate(similarity[movie_index])),key=lambda x:x[1],reverse=True)[1:6]
+
+if st.button("recommend"):
+    for i in similar_movies:
+        st.write(movies['title'][i[0]])
